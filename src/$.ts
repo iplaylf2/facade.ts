0eb21f4f585d034c1ef3190f7492915695d51ff7
i-play-lf2 <914637118@qq.com>
2022-04-12 13:59:44 +0000

    fix $

--- src/$.ts
+++ src/$.ts
@@ -11,65 +11,83 @@ const isPlaceholder = function (x: unknown): x is Placeholder {
 
 const let_currying = function (f: FunctionType, length: number): any {
   const anchor = function (this: unknown, ...args: unknown[]): any {
-    const preload: unknown[] = [];
-    const redirect: number[] = [];
-
-    for (let i = 0; length !== i && args.length !== i; i++) {
-      const current = args[i];
-      if (isPlaceholder(current)) {
-        redirect.push(i);
-        preload.push(undefined);
-      } else {
-        preload.push(current);
-      }
-    }
-
-    if (0 === redirect.length) {
-      if (length === preload.length) {
-        const result = f.apply(this, preload);
-        if (result instanceof Function) {
-          const restArgs = args.slice(length);
-          if (currying_function in result) {
-            return result.apply(this, restArgs);
-          } else {
-            return let_currying(result, result.length).apply(this, restArgs);
-          }
-        } else {
-          if (length === args.length) {
-            return result;
-          } else {
-            throw args;
-          }
-        }
-      } else {
-        if (0 === args.length) {
-          return anchor;
-        } else {
-          return let_currying(function (this: unknown, ...args) {
-            return f.call(this, ...preload, ...args);
-          }, length - preload.length);
-        }
-      }
+    if (0 !== length && 0 === args.length) {
+      return anchor;
     } else {
-      const restArgs = args.slice(preload.length);
-
-      return let_currying(function (this: unknown, ...args) {
-        const copy = preload.slice();
-        for (let i = 0; redirect.length !== i; i++) {
-          copy[redirect[i]!] = args[i];
-        }
-
-        return f.call(
-          this,
-          ...copy,
-          ...args.slice(redirect.length),
-          ...restArgs
-        );
-      }, length - preload.length + redirect.length);
+      return run_currying.call(this, f, length, args);
     }
   };
 
   return tag_currying(anchor);
+};
+
+const run_currying = function (
+  this: unknown,
+  f: FunctionType,
+  length: number,
+  args: unknown[]
+): any {
+  const preload: unknown[] = [];
+  const redirect: number[] = [];
+
+  for (let i = 0; length !== i && args.length !== i; i++) {
+    const current = args[i];
+    if (isPlaceholder(current)) {
+      redirect.push(i);
+      preload.push(undefined);
+    } else {
+      preload.push(current);
+    }
+  }
+
+  if (0 === redirect.length) {
+    if (length === preload.length) {
+      const result = f.apply(this, preload);
+      const restArgs = args.slice(length);
+      return apply_rest_argument.call(this, result, restArgs);
+    } else {
+      return let_currying(function (this: unknown, ...args) {
+        return f.call(this, ...preload, ...args);
+      }, length - preload.length);
+    }
+  } else {
+    const restArgs = args.slice(preload.length);
+
+    return let_currying(function (this: unknown, ...args) {
+      const copy = preload.slice();
+      for (let i = 0; redirect.length !== i; i++) {
+        copy[redirect[i]!] = args[i];
+      }
+
+      const result = f.call(this, ...copy, ...args.slice(redirect.length));
+      return apply_rest_argument.call(this, result, restArgs);
+    }, length - preload.length + redirect.length);
+  }
+};
+
+const apply_rest_argument = function (
+  this: unknown,
+  result: unknown,
+  restArgs: unknown[]
+): any {
+  if (result instanceof Function) {
+    if (currying_function in result) {
+      return result.apply(this, restArgs);
+    } else {
+      return run_currying.call(
+        this,
+        result as FunctionType,
+        result.length,
+        restArgs
+      );
+    }
+  } else {
+    if (0 === restArgs.length) {
+      return result;
+    } else {
+      throw restArgs;
+    }
+  }
 };
 
 const tag_currying = function (f: FunctionType) {
@@ -78,19 +96,20 @@ const tag_currying = function (f: FunctionType) {
   });
 };
 
-export const $ = Object.assign(
-  <T extends FunctionType>(f: T): Currying<T> => {
-    if (currying_function in f) {
-      return f;
-    } else {
-      if (0 === f.length) {
-        throw f;
+export const $: (<T extends FunctionType>(f: T) => Currying<T>) & Placeholder =
+  Object.assign(
+    <T extends FunctionType>(f: T) => {
+      if (currying_function in f) {
+        return f;
       } else {
-        return let_currying(f, f.length);
+        if (0 === f.length) {
+          throw f;
+        } else {
+          return let_currying(f, f.length);
+        }
       }
+    },
+    {
+      [placeholder]: true,
     }
-  },
-  {
-    [placeholder]: true,
-  } as Placeholder
-);
+  );
