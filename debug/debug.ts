c8cc1a3c87419693bec8149f55ca651f566da49a
i-play-lf2 <914637118@qq.com>
2022-04-17 11:08:45 +0000

    remove \r

--- debug/debug.ts
+++ debug/debug.ts
@@ -1,7 +1,14 @@
-import { $, $class, margin } from "facade.ts";
-import { Currying } from "facade.ts/type/function";
+import { $, $class, flip, margin } from "facade.ts";
 
 const delay = (ms: number): Promise<void> => {
-  const delayCb = $(setTimeout, 2)($, ms);
-  return $class(Promise)(flip(margin(delayCb, 1)));
+  const delay_cb = flip(margin($(setTimeout, 2)($, ms), 1));
+  return $class(Promise)(delay_cb) as any;
 };
+
+const delayX = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+console.log(performance.now());
+delay(1000)
+  .then(() => console.log("zzz"))
+  .then(() => console.log(performance.now()));
