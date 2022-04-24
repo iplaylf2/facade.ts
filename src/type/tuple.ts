38288b26d648e20a15f713cabb129d5606ba885a
i-play-lf2 <914637118@qq.com>
2022-04-24 10:48:56 +0000

    improve

--- src/type/tuple.ts
+++ src/type/tuple.ts
@@ -11,8 +11,8 @@ export type IsOptionalTuple<T extends unknown[]> = T extends [
     : false
   : false;
 
-export type Repeat<T extends unknown, N extends number, R extends T[] = []> = [
-  R["length"]
-] extends [N]
-  ? R
-  : Repeat<T, N, [...R, T]>;
+export type Repeat<
+  T extends unknown,
+  N extends number,
+  R extends T[] = []
+> = R["length"] extends N ? R : Repeat<T, N, [...R, T]>;
